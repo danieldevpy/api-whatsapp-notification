57e87f5a0a644c6ab69a52025bd2231fab0e7481
Daniel Fernandes <danielfernandes202@gmail.com>
2024-02-28 14:19:30 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Request, Form, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from controller.wpp import WhatsappNotification, ChromeDriverController
+from controller.list_contacts import ListContacts
+from pydantic import BaseModel
+
+
+class RequestWpp(BaseModel):
+    number: str
+    message: str
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+driver = ChromeDriverController(False, True)
+contacts = ListContacts()
+wpp = WhatsappNotification(driver, contacts)
+
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "contatos": contacts.get_contacts_list()})
+
+@app.get('/contatos')
+def get_contatos():
+    return {"contatos": contacts.get_contacts_list()}
+
+@app.post('/send')
+def send_message(data: RequestWpp):
+  contact = contacts.get_contact(data.number)
+  if not contact:
+     return JSONResponse({"msg": "contato não encontrado"}, 400)
+  wpp.send_message(contact, data.message)
+  return f'A mensagem foi enviada'
+
+@app.post('/sendNew')
+def send_new_contact(data: RequestWpp):
+    wpp.send_message_new_contact(data.number, data.message)
+
+@app.post('/sendForm')
+def send_message(request: Request, contato: str = Form(None), message: str = Form(None)):
+    contact = contacts.get_contact(contato)
+    msg = None
+    if not contact:
+        msg = 'contato não encontrado'
+    wpp.send_message(contact, message)
+    if msg == None:
+        msg = 'a mensagem foi enviada'
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "contatos": contacts.get_contacts_list(),
+        "alert": msg
+    })
